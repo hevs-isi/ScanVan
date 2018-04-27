99f60add53253cbb52c84e750682eace2569fbb6
Marcelo Kaihara <marcelo.kaihara@hevs.ch>
2018-04-27 14:59:44 +0200

    Writes attributes of the camera into file

--- Grab_UsingActionCommand/Grab_UsingActionCommand.cpp
+++ Grab_UsingActionCommand/Grab_UsingActionCommand.cpp
@@ -69,11 +69,11 @@ const int offsetY = 2;
 
 // Image buffers
 const unsigned long int imageBufferSize = imageWidth * imageHeight; // Unpacked 8-bit per pixel
-uint8_t rawImageBuffer1[imageBufferSize];
+uint8_t rawImageBuffer[imageBufferSize];
 
 // Flags to control access to the buffers
-bool buffer1PylonReadable = false;
-bool buffer1RawReadable = false;
+bool bufferOpenCvReadable = false;
+bool bufferRawReadable = false;
 
 // Barrier to synchronize the threads
 pthread_barrier_t thBarrier;
@@ -147,7 +147,7 @@ int main(int argc, char* argv[])
 
 	pthread_barrier_init(&thBarrier, NULL, THREAD_BARRIER_COUNT);
 
-	double expTime[2] { 0, 0};
+	double expTime[2] {0, 0};
 	int gain[2] {0, 0};
 
 	if (readConfigFile (expTime[0], gain[0], expTime[1], gain[1])) {
@@ -306,14 +306,14 @@ int main(int argc, char* argv[])
 		int key = 0;
 
 		while (!terminateLoop) {
-			if (buffer1PylonReadable) {
+			if (bufferOpenCvReadable) {
 
 				cout << "thread " << thread_id << ": Start displaying image number " << imageNum << endl;
 
 				//intptr_t cameraIndex = ptrGrabResult1->GetCameraContext();
 				const int cameraIndex = 0;
 
-				openCvImageRG8 = cv::Mat(imageHeight, imageWidth, CV_8UC1, rawImageBuffer1);
+				openCvImageRG8 = cv::Mat(imageHeight, imageWidth, CV_8UC1, rawImageBuffer);
 
 				cvtColor(openCvImageRG8, openCvImage, COLOR_BayerRG2RGB);
 
@@ -471,34 +471,33 @@ void* thCapture (void *threadarg) {
 
 	const int DefaultTimeout_ms = 10000;
 
-	CImageFormatConverter formatConverter;
-	// Specify the output pixel format.
-	formatConverter.OutputPixelFormat = PixelType_BGR8packed;
-
 	passedData->cameras->StartGrabbing();
 
+	image_data imData[2]; // Struct where information of the images are stored
+
+	cout << "thread " << thread_id << ": Start getting parameters from camera " << endl;
+
+	// Get the information about the cameras and store temporarily in a struct
+	for (size_t i = 0; i < (*(passedData->cameras)).GetSize(); ++i) {
+		imData[i].cameraIdx = i;
+		imData[i].exposureTime =	(*(passedData->cameras))[i].ExposureTimeAbs.GetValue();
+		imData[i].gain = (*(passedData->cameras))[i].GainRaw.GetValue();
+		(*(passedData->cameras))[i].BalanceRatioSelector.SetValue(BalanceRatioSelector_Red);
+		imData[i].balanceR = (*(passedData->cameras))[i].BalanceRatioAbs.GetValue();
+		(*(passedData->cameras))[i].BalanceRatioSelector.SetValue(BalanceRatioSelector_Green);
+		imData[i].balanceG = (*(passedData->cameras))[i].BalanceRatioAbs.GetValue();
+		(*(passedData->cameras))[i].BalanceRatioSelector.SetValue(BalanceRatioSelector_Blue);
+		imData[i].balanceB = (*(passedData->cameras))[i].BalanceRatioAbs.GetValue();
+		imData[i].autoExpTime = (int) autoExpTimeCont;
+		imData[i].autoGain = (int) autoGainCont;
+	}
+
+	// Wait for trigger ready
 	((*(passedData->cameras))[0].WaitForFrameTriggerReady(10000, TimeoutHandling_ThrowException));
+	// Isuue an action command
 	passedData->pTL->IssueActionCommand(passedData->DeviceKey, passedData->GroupKey, AllGroupMask, passedData->subnet);
 
 	while (!terminateLoop) {
-
-//				cout << endl << "Issuing an action command." << endl;
-
-//		cout << "thread " << thread_id << ": Start getting parameters from camera " <<  endl;
-//		for (size_t i = 0; i < (*(passedData->cameras)).GetSize(); ++i) {
-//			imageData[i].exposureTime = (*(passedData->cameras))[i].ExposureTimeAbs.GetValue();
-//			imageData[i].gain = (*(passedData->cameras))[i].GainRaw.GetValue();
-//			(*(passedData->cameras))[i].BalanceRatioSelector.SetValue(BalanceRatioSelector_Red);
-//			imageData[i].balanceR = (*(passedData->cameras))[i].BalanceRatioAbs.GetValue();
-//			(*(passedData->cameras))[i].BalanceRatioSelector.SetValue(BalanceRatioSelector_Green);
-//			imageData[i].balanceG = (*(passedData->cameras))[i].BalanceRatioAbs.GetValue();
-//			(*(passedData->cameras))[i].BalanceRatioSelector.SetValue(BalanceRatioSelector_Blue);
-//			imageData[i].balanceB = (*(passedData->cameras))[i].BalanceRatioAbs.GetValue();
-//			imageData[i].autoExpTime = (int) autoExpTimeCont;
-//			imageData[i].autoGain = (int) autoGainCont;
-//
-//		}
-
 
 		// This smart pointer will receive the grab result data.
 		CBaslerGigEGrabResultPtr ptrGrabResult;
@@ -506,30 +505,17 @@ void* thCapture (void *threadarg) {
 		// Retrieve images from all cameras.
 		for (size_t i = 0; i < passedData->usableDeviceInfos->size() && passedData->cameras->IsGrabbing(); ++i) {
 
-			try {
+			cout << "thread " << thread_id << ": Retrieve result" << endl;
+			// CInstantCameraArray::RetrieveResult will return grab results in the order they arrive.
+			passedData->cameras->RetrieveResult(DefaultTimeout_ms, ptrGrabResult, TimeoutHandling_ThrowException);
+			cout << "thread " << thread_id << ": End retrieve result" << endl;
 
-				cout << "thread " << thread_id << ": Retrieve result" << endl;
-				// CInstantCameraArray::RetrieveResult will return grab results in the order they arrive.
-				passedData->cameras->RetrieveResult(DefaultTimeout_ms, ptrGrabResult, TimeoutHandling_ThrowException);
-				cout << "thread " << thread_id << ": End retrieve result" << endl;
-
-			} catch (const GenericException &e) {
-				// Error handling
-				cout << "ERROR!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" << endl;
-				cerr << "An exception occurred." << endl << e.GetDescription() << endl;
-			}
-
-			// When the cameras in the array are created the camera context value
-			// is set to the index of the camera in the array.
-			// The camera context is a user-settable value.
-			// This value is attached to each grab result and can be used
-			// to determine the camera that produced the grab result.
 			cout << "thread " << thread_id << ": Get camera index" << endl;
 			intptr_t cameraIndex = ptrGrabResult->GetCameraContext();
 			cout << "thread " << thread_id << ": Got camera index" << endl;
 
 			auto tnow = std::chrono::system_clock::now();
-			imageData[cameraIndex].captureTime = std::chrono::system_clock::to_time_t(tnow);
+			imData[cameraIndex].captureTime = std::chrono::system_clock::to_time_t(tnow);
 
 			pthread_barrier_wait(&thBarrier);
 
@@ -541,13 +527,26 @@ void* thCapture (void *threadarg) {
 				cout << "thread " << thread_id << ": Start writing image number " << numGrabbedImages << endl;
 
 				// Copy image to raw image buffer1
-				memcpy(rawImageBuffer1, pImageBuffer, imageBufferSize);
+				memcpy(rawImageBuffer, pImageBuffer, imageBufferSize);
 
-				buffer1PylonReadable = true;
-				buffer1RawReadable = true;
+				bufferOpenCvReadable = true;
+				bufferRawReadable = true;
 				cout << "thread " << thread_id << ": Finished writing buffer." << endl;
 
 				numGrabbedImages++;
+
+				cout << "thread " << thread_id << ": Start getting parameters from camera " << endl;
+
+				// Save the attributes into a global struct so that it can be saved from a different thread
+				imageData[i].cameraIdx = imData[i].cameraIdx;
+				imageData[i].exposureTime =	imData[i].exposureTime;
+				imageData[i].gain = imData[i].gain;
+				imageData[i].balanceR = imData[i].balanceR;
+				imageData[i].balanceG = imData[i].balanceG;
+				imageData[i].balanceB = imData[i].balanceB;
+				imageData[i].autoExpTime = imData[i].autoExpTime;
+				imageData[i].autoGain = imData[i].autoGain;
+				imageData[i].captureTime = imData[i].captureTime;
 
 				pthread_barrier_wait(&thBarrier);
 
@@ -557,188 +556,37 @@ void* thCapture (void *threadarg) {
 				cout << "Error: " << ptrGrabResult->GetErrorCode() << " "
 						<< ptrGrabResult->GetErrorDescription() << endl;
 				numGrabError++;
-				buffer1RawReadable = false;
+				bufferRawReadable = false;
 			}
 
 		}
 
+		// Get the information about the cameras and store temporarily in a struct
+		for (size_t i = 0; i < (*(passedData->cameras)).GetSize(); ++i) {
+			imData[i].cameraIdx = i;
+			imData[i].exposureTime =	(*(passedData->cameras))[i].ExposureTimeAbs.GetValue();
+			imData[i].gain = (*(passedData->cameras))[i].GainRaw.GetValue();
+			(*(passedData->cameras))[i].BalanceRatioSelector.SetValue(BalanceRatioSelector_Red);
+			imData[i].balanceR = (*(passedData->cameras))[i].BalanceRatioAbs.GetValue();
+			(*(passedData->cameras))[i].BalanceRatioSelector.SetValue(BalanceRatioSelector_Green);
+			imData[i].balanceG = (*(passedData->cameras))[i].BalanceRatioAbs.GetValue();
+			(*(passedData->cameras))[i].BalanceRatioSelector.SetValue(BalanceRatioSelector_Blue);
+			imData[i].balanceB = (*(passedData->cameras))[i].BalanceRatioAbs.GetValue();
+			imData[i].autoExpTime = (int) autoExpTimeCont;
+			imData[i].autoGain = (int) autoGainCont;
+		}
+
+		// Wait for trigger ready
 		((*(passedData->cameras))[0].WaitForFrameTriggerReady(10000, TimeoutHandling_ThrowException));
+		// Issue an action command
 		passedData->pTL->IssueActionCommand(passedData->DeviceKey, passedData->GroupKey, AllGroupMask, passedData->subnet);
 
-		// In case you want to trigger again you should wait for the camera
-		// to become trigger-ready before issuing the next action command.
-		// To avoid overtriggering you should call cameras[0].WaitForFrameTriggerReady
-		// (see Grab_UsingGrabLoopThread sample for details).
-//		try {
-//			cout << "thread " << thread_id << ": Start waiting trigger ready" <<  endl;
-//			for (size_t i = 0; i < passedData->cameras->GetSize(); ++i) {
-//				(*(passedData->cameras))[0].WaitForFrameTriggerReady(10000,	TimeoutHandling_ThrowException);
-//			}
-//			cout << "thread " << thread_id << ": Finish waiting trigger ready" <<  endl;
-//		} catch (const GenericException &e) {
-//			// Error handling
-//			cout << "ERROR===============================================" << endl;
-//			cerr << "An exception occurred." << endl << e.GetDescription() << endl;
-//		}
-
 	}
 
 	passedData->cameras->StopGrabbing();
 
 	pthread_exit(NULL);
 }
-
-
-/*
-void* thCapture (void *threadarg) {
-
-	struct thread_data *passedData;
-	passedData = (struct thread_data *) threadarg;
-
-	int thread_id = passedData->thread_id;
-
-	// Starts grabbing for all cameras.
-	// The cameras won't transmit any image data, because they are configured to wait for an action command.
-	passedData->cameras->StartGrabbing();
-
-	const int DefaultTimeout_ms = 10000;
-
-	CImageFormatConverter formatConverter;
-	// Specify the output pixel format.
-	formatConverter.OutputPixelFormat = PixelType_BGR8packed;
-
-	while ((!terminateLoop) && (passedData->cameras->IsGrabbing())) {
-
-		cout << "thread " << thread_id << ": Start grabbing image number " << numGrabbedImages << endl;
-
-		//////////////////////////////////////////////////////////////////////
-		//////////////////////////////////////////////////////////////////////
-		// Use an Action Command to Trigger Multiple Cameras at the Same Time.
-		//////////////////////////////////////////////////////////////////////
-		//////////////////////////////////////////////////////////////////////
-
-//				cout << endl << "Issuing an action command." << endl;
-
-//		cout << "thread " << thread_id << ": Start getting parameters from camera " <<  endl;
-//		for (size_t i = 0; i < (*(passedData->cameras)).GetSize(); ++i) {
-//			imageData[i].exposureTime = (*(passedData->cameras))[i].ExposureTimeAbs.GetValue();
-//			imageData[i].gain = (*(passedData->cameras))[i].GainRaw.GetValue();
-//			(*(passedData->cameras))[i].BalanceRatioSelector.SetValue(BalanceRatioSelector_Red);
-//			imageData[i].balanceR = (*(passedData->cameras))[i].BalanceRatioAbs.GetValue();
-//			(*(passedData->cameras))[i].BalanceRatioSelector.SetValue(BalanceRatioSelector_Green);
-//			imageData[i].balanceG = (*(passedData->cameras))[i].BalanceRatioAbs.GetValue();
-//			(*(passedData->cameras))[i].BalanceRatioSelector.SetValue(BalanceRatioSelector_Blue);
-//			imageData[i].balanceB = (*(passedData->cameras))[i].BalanceRatioAbs.GetValue();
-//			imageData[i].autoExpTime = (int) autoExpTimeCont;
-//			imageData[i].autoGain = (int) autoGainCont;
-//
-//		}
-
-		// Now we issue the action command to all devices in the subnet.
-		// The devices with a matching DeviceKey, GroupKey and valid GroupMask will grab an image.
-		cout << "thread " << thread_id << ": Issue and action command" <<  endl;
-		if ((*(passedData->cameras))[0].WaitForFrameTriggerReady(10000,	TimeoutHandling_ThrowException))
-			passedData->pTL->IssueActionCommand(passedData->DeviceKey, passedData->GroupKey, AllGroupMask, passedData->subnet);
-		else
-			cout << "Camera is not trigger ready" << endl;
-
-		// This smart pointer will receive the grab result data.
-		CBaslerGigEGrabResultPtr ptrGrabResult;
-
-		// Retrieve images from all cameras.
-		for (size_t i = 0; i < passedData->usableDeviceInfos->size() && passedData->cameras->IsGrabbing(); ++i) {
-
-			try {
-				cout << "thread " << thread_id << ": Retrieve result" <<  endl;
-				// CInstantCameraArray::RetrieveResult will return grab results in the order they arrive.
-				passedData->cameras->RetrieveResult(DefaultTimeout_ms, ptrGrabResult, TimeoutHandling_ThrowException);
-				cout << "thread " << thread_id << ": End retrieve result" <<  endl;
-			} catch (const GenericException &e) {
-				// Error handling
-				cout << "ERROR!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" << endl;
-				cerr << "An exception occurred." << endl << e.GetDescription() << endl;
-			}
-
-			// When the cameras in the array are created the camera context value
-			// is set to the index of the camera in the array.
-			// The camera context is a user-settable value.
-			// This value is attached to each grab result and can be used
-			// to determine the camera that produced the grab result.
-			cout << "thread " << thread_id << ": Get camera index" <<  endl;
-			intptr_t cameraIndex = ptrGrabResult->GetCameraContext();
-			cout << "thread " << thread_id << ": Got camera index" <<  endl;
-
-			auto tnow = std::chrono::system_clock::now();
-			imageData[cameraIndex].captureTime = std::chrono::system_clock::to_time_t(tnow);
-
-			pthread_barrier_wait(&thBarrier);
-
-			// Image grabbed successfully?
-			if (ptrGrabResult->GrabSucceeded()) {
-
-//						// Print the index and the model name of the camera.
-//						cout << "Camera " << cameraIndex << ": "
-//								<< (*(passedData->cameras))[cameraIndex].GetDeviceInfo().GetModelName()
-//								<< " ("
-//								<< (*(passedData->cameras))[cameraIndex].GetDeviceInfo().GetIpAddress()
-//								<< ")" << endl;
-//
-//						// You could process the image here by accessing the image buffer.
-//						cout << "GrabSucceeded: " << ptrGrabResult->GrabSucceeded() << endl;
-
-				const uint8_t *pImageBuffer =
-						(uint8_t *) ptrGrabResult->GetBuffer();
-//
-//						cout << "Gray value of first pixel: " << (uint32_t) pImageBuffer[0] << endl;
-//						cout << "Image number: " << numGrabbedImages << endl << endl;
-
-				cout << "thread " << thread_id << ": Start writing image number " << numGrabbedImages << endl;
-
-				// Copy image to raw image buffer1
-				memcpy(rawImageBuffer1, pImageBuffer, imageBufferSize);
-
-				buffer1PylonReadable = true;
-				buffer1RawReadable = true;
-				cout << "thread " << thread_id << ": Finished writing buffer." << endl;
-
-			} else {
-				// If a buffer has been incompletely grabbed, the network bandwidth is possibly insufficient for transferring
-				// multiple images simultaneously. See note above c_maxCamerasToUse.
-				cout << "Error: " << ptrGrabResult->GetErrorCode() << " " << ptrGrabResult->GetErrorDescription() << endl;
-				numGrabError++;
-				buffer1RawReadable = false;
-			}
-		}
-
-		numGrabbedImages++;
-
-		// In case you want to trigger again you should wait for the camera
-		// to become trigger-ready before issuing the next action command.
-		// To avoid overtriggering you should call cameras[0].WaitForFrameTriggerReady
-		// (see Grab_UsingGrabLoopThread sample for details).
-//		try {
-//			cout << "thread " << thread_id << ": Start waiting trigger ready" <<  endl;
-//			for (size_t i = 0; i < passedData->cameras->GetSize(); ++i) {
-//				(*(passedData->cameras))[0].WaitForFrameTriggerReady(10000,	TimeoutHandling_ThrowException);
-//			}
-//			cout << "thread " << thread_id << ": Finish waiting trigger ready" <<  endl;
-//		} catch (const GenericException &e) {
-//			// Error handling
-//			cout << "ERROR===============================================" << endl;
-//			cerr << "An exception occurred." << endl << e.GetDescription() << endl;
-//		}
-
-		cout << "thread " << thread_id << ": Finished grabbing." << endl;
-
-		pthread_barrier_wait(&thBarrier);
-
-	}
-
-	passedData->cameras->StopGrabbing();
-
-	pthread_exit(NULL);
-}
-*/
 
 void* thSave (void *threadarg) {
 
@@ -751,7 +599,7 @@ void* thSave (void *threadarg) {
 
 	while (!terminateLoop) {
 
-		if (buffer1RawReadable) {
+		if (bufferRawReadable) {
 
 			cout << "thread " << thread_id << ": Start saving image number " << imageNum << endl;
 
@@ -771,7 +619,7 @@ void* thSave (void *threadarg) {
 			// Save the raw image into file
 			ofstream myFile (imageName, ios::out | ios::binary);
 			if (myFile.is_open()) {
-				myFile.write ((char*)rawImageBuffer1,imageBufferSize);
+				myFile.write ((char*)rawImageBuffer,imageBufferSize);
 				myFile.close();
 			} else {
 				cout << "Error writing image file " << imageName << endl;
