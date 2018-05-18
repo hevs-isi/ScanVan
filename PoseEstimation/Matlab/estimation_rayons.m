8bdbd00265683a531a1cc24ce27bffca0d6c1c8a
mkaihara <marcelo.kaihara@gmail.com>
2018-05-18 15:32:59 +0200

    Correction of the rotation

--- PoseEstimation/Matlab/estimation_rayons.m
+++ PoseEstimation/Matlab/estimation_rayons.m
@@ -4,6 +4,7 @@ function [ sv_u, sv_v, sv_w ] = estimation_rayons(p3d_1, p3d_2, p3d_3, sv_r, sv_
 longueur = size(p3d_1,1);
 
 sv_r_12 = sv_r(1:3,1:3);
+sv_r_23 = sv_r(1:3,4:6);
 sv_r_31 = sv_r(1:3,7:9);
 
 sv_t_12 = sv_t(1:3);
@@ -18,8 +19,7 @@ sv_v = zeros(longueur,1);
 sv_w = zeros(longueur,1);
 
 azim1m = p3d_1;
-%azim2m = p3d_2 * sv_r_23;
-azim2m = p3d_2 * sv_r_31;
+azim2m = (p3d_2 * sv_r_23) * sv_r_31;
 azim3m = p3d_3 * sv_r_31;
 
 for i=1:longueur
--- PoseEstimation/Matlab/pose_scene.m
+++ PoseEstimation/Matlab/pose_scene.m
@@ -3,6 +3,7 @@ function [ sv_scene ] = pose_scene( p3d_1, p3d_2, p3d_3, sv_r, sv_t )
 
 longueur = size(p3d_1,1);
 sv_r_12 = sv_r(1:3,1:3);
+sv_r_23 = sv_r(1:3,4:6);
 sv_r_31 = sv_r(1:3,7:9);
 sv_t_12 = sv_t(1:3);
 sv_t_23 = sv_t(4:6);
@@ -14,7 +15,7 @@ c3 = c2 + (sv_r_12 *sv_t_23')';
 sv_scene = zeros(longueur,3);
 
 azim1m = p3d_1;
-azim2m = p3d_2 * sv_r_31;
+azim2m = (p3d_2 * sv_r_23) * sv_r_31;
 azim3m = p3d_3 * sv_r_31;
 
 for i=1:longueur    
